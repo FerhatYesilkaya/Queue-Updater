70fb0af2ede2306d35eff72ef15b2bd76e4b02fb
FerhatYesilkaya <ferhat-20001@hotmail.de>
2024-05-09 13:55:17 +0200

    reading current selected view

--- background.js
+++ background.js
@@ -1,15 +1,20 @@
+let currentSelectedView;
+var timer = 10000;
+
 function clickSpanPeriodically() {
   chrome.storage.sync.get('selectedDropDownOption', (data) => { // Ändern Sie den Key auf 'selectedDropDownOption'
     const selectedOption = data.selectedDropDownOption;
+    console.log(timer);
+    console.log('Text des Span-Elements3:', currentSelectedView);
     if (selectedOption !== undefined) {
-      const selectedIndex = selectedOption.index; // Ändern Sie den Namen des Indexattributs entsprechend Ihrer Speicherstruktur
-      clickSpanAtIndex(selectedIndex); // Rufen Sie die Funktion zum Klicken auf das Span-Element mit dem ausgewählten Index auf
+        const selectedIndex = selectedOption.index; // Ändern Sie den Namen des Indexattributs entsprechend Ihrer Speicherstruktur
+        clickSpanAtIndex(selectedIndex); // Rufen Sie die Funktion zum Klicken auf das Span-Element mit dem ausgewählten Index auf
     }
   });
 }
 
-// Periodisches Ausführen der Klickfunktion alle 20 Sekunden
-setInterval(clickSpanPeriodically, 10000);
+// Periodisches Ausführen der Klickfunktion alle 10 Sekunden
+setInterval(clickSpanPeriodically, timer);
 
 // Funktion zum Klicken auf das definierte Span-Element basierend auf dem empfangenen Index
 function clickSpanAtIndex(index) {
@@ -30,4 +35,32 @@ function clickSpanAtIndex(index) {
       });
     });
   });
+
 }
+
+function getTextFromSpanPeriodically() {
+  chrome.tabs.query({}, (tabs) => {
+    tabs.forEach((tab) => {
+      const tabId = tab.id;
+      chrome.scripting.executeScript({
+        target: { tabId: tabId },
+        function: () => {
+          const spans = document.querySelectorAll('span.selectedListView');
+          if (spans.length > 0) {
+            const spanText = spans[0].textContent;
+            // Hier können Sie den Text des Span-Elements verwenden oder weitere Verarbeitungen durchführen
+            currentSelectedView = spanText;
+            console.log(currentSelectedView);
+            console.log(timer);
+          } else {
+            currentSelectedView = "Fehler";
+            console.error('Kein Span-Element mit der angegebenen Klasse gefunden.');
+          }
+        }
+      });
+    });
+  });
+}
+
+// Periodisches Ausführen der Funktion alle 10 Sekunden
+setInterval(getTextFromSpanPeriodically, (timer-100));
